4817f4fa7ea282ac732e35a503f7cb2e267eba82
STICKAL <1986767919@qq.com>
2019-12-02 22:58:08 +0800

    '推荐信息和商品数据的请求'

---
+++ src/components/content/tabcontroller/TabController.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="tab-control">
+        <div v-for="(item,index) in titles"
+             class="tab-control-item"
+             :class="{active:index === currentIndex}" @click="itemclick(index)">
+            <span>{{item}}</span>
+        </div>
+    </div>
+</template>
+
+<script type="text/ecmascript-6">
+    export default {
+        name: 'TabController',
+        props:{
+            titles:{
+                type:Array,
+                default:function(){
+                    return []
+                }
+            }
+        },
+        data:function(){
+            return {
+                currentIndex:0
+            }
+        },
+        methods:{
+            itemclick: function (index) {
+                this.currentIndex = index;
+            }
+        }
+
+    }
+</script>
+
+<style>
+    .tab-control{
+        display: flex;
+        text-align: center;
+        height: 40px;
+        line-height: 40px;
+        font-size: 16px;
+        background-color: #fff;
+    }
+
+    .tab-control-item{
+        flex: 1;
+    }
+
+    .tab-control-item span{
+        padding: 5px;
+    }
+
+    .active{
+        color: var(--color-high-text);
+
+    }
+
+    .active span{
+        border-bottom: 3px solid var(--color-tint);
+    }
+</style>
---
+++ src/views/home/childcomponents/RecommendView.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="recommend">
+        <div v-for="item in recommends" class="recommend-item">
+            <a :href="item.link">
+                <img :src="item.image" alt=""/>
+                <div>{{item.title}}</div>
+            </a>
+        </div>
+    </div>
+</template>
+
+<script type="text/ecmascript-6">
+    export default {
+        name: 'RecommendView',
+        props:{
+            recommends:{
+                type:Array,
+                default:function(){
+                    return []
+                 }
+            }
+        }
+    }
+</script>
+
+<style>
+   /*设置推荐的样式*/
+    .recommend{
+        display: flex;
+        width: 100%;
+
+        text-align: center;
+        font-size: 12px;
+
+        padding: 15px 0 20px;
+
+        border-bottom: 10px solid #eee;
+    }
+
+    .recommend-item{
+        flex: 1;
+    }
+
+    .recommend-item img{
+        width: 70px;
+        height: 70px;
+
+        /*设置文字距离底部的位置*/
+        margin-bottom:10px ;
+    }
+</style>
